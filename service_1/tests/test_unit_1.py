b6701959b68dd2d05adeef66095f21804eeaa847
gavthetallone <gavinvw123@gmail.com>
2021-08-09 14:35:18 +0000

    GW: Updated unit test for service 1

---
+++ service_1/tests/test_unit_1.py
@@ -0,0 +1,33 @@
+from flask import url_for
+from flask_testing import TestCase
+from requests_mock import mock
+
+from app import app, db
+
+class TestBase(TestCase):
+    def create_app(self):
+        app.config.update(
+            SQLALCHEMY_DATABASE_URI="sqlite:///"
+        )
+        return app
+    
+    def setUp(self):
+        db.create_all()
+    
+    def tearDown(self):
+        db.drop_all()
+    
+class TestResponse(TestBase):
+
+    def test_home(self):
+
+        with mock() as m:
+            m.get('http://service-2:5000/get/region', text='Hoenn')
+            m.get('http://service-3:5000/get/pokemon_type', text='Water')
+            m.post('http://service-4:5000/post/name', text='Mudkip')
+
+            response = self.client.get(url_for('home'))
+
+        self.assert200(response)
+
+        self.assertIn('Mudkip', response.data.decode())
