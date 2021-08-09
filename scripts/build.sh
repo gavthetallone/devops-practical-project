7b503c706c85aed4e4b67b8dbd470cbcafcf560a
gavthetallone <gavinvw123@gmail.com>
2021-08-09 20:44:48 +0000

    GW: Modified build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-docker-compose build --parallel && \
+set -e
+
+# Build images
+docker-compose build --parallel
+
+# Push images
+docker login -u ${DOCKER_CREDENTIALS_USR} -p ${DOCKER_CREDENTIALS_PSW}
 docker-compose push
