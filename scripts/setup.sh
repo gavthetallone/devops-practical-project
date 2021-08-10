f96498dd8f4eeb628a7739755b008123097d7a54
gavthetallone <gavinvw123@gmail.com>
2021-08-10 10:01:10 +0000

    GW: Updated Ansible installation

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y curl jq
 
 sudo apt-get install python3-dev libpq-dev software-properties-common
 
-sudo apt-add repository -y --update ppa:ansible/ansible
+sudo apt-add-repository -y --update ppa:ansible/ansible
 sudo apt-get install -y ansible
 
 if [ -z "$(docker --version 2> /dev/null)" ]; then
