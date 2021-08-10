709b0f11cfcd4db6dc44f2a9f5e5c8b693553902
gavthetallone <gavinvw123@gmail.com>
2021-08-10 10:09:45 +0000

    GW: Updated deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,6 @@
 rsync -r docker-compose.yaml nginx swarm-master:
 ssh swarm-master << EOF
     sudo usermod -aG docker $USER
+    docker login -u ${DOCKER_CREDENTIALS_USR} -p ${DOCKER_CREDENTIALS_PSW}
     docker stack deploy --compose-file docker-compose.yaml pokemon-app
 EOF
