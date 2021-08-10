09488815db9875044a60bb7e7a575c7b57cdc1a1
gavthetallone <gavinvw123@gmail.com>
2021-08-10 11:00:11 +0000

    GW: Updated deloy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-rsync -r docker-compose.yaml nginx_lb/nginx.conf swarm-master:
+rsync -r docker-compose.yaml swarm-master:
+rsync -r nginx_lb/nginx.conf remote:~/nginx/ swarm-master:
 ssh swarm-master << EOF
     sudo usermod -aG docker $USER
     docker login -u ${DOCKER_CREDENTIALS_USR} -p ${DOCKER_CREDENTIALS_PSW}
