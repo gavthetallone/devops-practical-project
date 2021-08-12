daa59f80a1397124803cfc9998179e29d2524419
gavthetallone <gavinvw123@gmail.com>
2021-08-12 16:51:25 +0000

    GW: Updated regions and types

--- service_1/application/forms.py
+++ service_1/application/forms.py
@@ -2,7 +2,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 
 class PokeForm(FlaskForm):
-    poke_type = SelectField("Filter by type:", choices=["All", "Electric", "Fire", "Water", "Grass"])
-    poke_region = SelectField("Filter by region:", choices=["All", "Kanto", "Johto", "Hoenn", "Sinnoh"])
+    # poke_type = SelectField("Filter by type:", choices=["All", "Electric", "Fire", "Water", "Grass"])
+    poke_type = SelectField("Filter by type:", choices=["All", "Electric", "Psychic", "Fighting", "Dragon"])
+    # poke_region = SelectField("Filter by region:", choices=["All", "Kanto", "Johto", "Hoenn", "Sinnoh"])
+    poke_region = SelectField("Filter by region:", choices=["All", "Kanto", "Unova", "Kalos", "Alola"])
     poke_order = SelectField("Order by:", choices=["", "#", "Name"])
     submit = SubmitField("Submit")
