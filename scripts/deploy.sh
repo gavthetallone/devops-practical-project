d9e2602610a706dc0b6034d4d124885fb5783771
gavthetallone <gavinvw123@gmail.com>
2021-08-13 13:01:37 +0000

    GW: Updated deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,5 +9,4 @@ ssh swarm-master << EOF
     sudo usermod -aG docker $USER
     docker login -u ${DOCKER_CREDENTIALS_USR} -p ${DOCKER_CREDENTIALS_PSW}
     docker stack deploy --compose-file docker-compose.yaml pokemon-app
-    docker stack deploy --compose-file docker-compose.yaml pokemon-app
 EOF
